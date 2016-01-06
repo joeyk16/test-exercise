df43872dd2c698f9cb621b07def6fbfed77066d9
joeyk16 <drknoba@hotmail.com>
2016-01-06 21:01:06 +1000

    fixed conflicts

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,23 +3,19 @@ class Product < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :category
-<<<<<<< HEAD
-=======
+
   has_many :outfit_products
   has_many :outfits, through: :outfit_products
->>>>>>> master
 
   has_many :product_sizes
   has_many :product_images, :dependent => :destroy
 
-<<<<<<< HEAD
   validates :title, presence: true, length: { maximum: 30 }
   validates :description, presence: true, length: { maximum: 2000 }
   validates :category, :user, :price, presence: true
 
   accepts_nested_attributes_for :product_images, :product_sizes, allow_destroy: true
-=======
+
   has_attached_file :image, styles: { large: "600x600", medium: "250x250", thumb:"100x100#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
->>>>>>> master
 end
