e2714759f770f661e13654787833bacf9d9180e8
Joseph Konop <drknoba@hotmail.com>
2016-01-19 19:05:15 +1000

    making edit work

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,29 +2,42 @@ class ProductsController < ApplicationController
   before_action :set_product, only: [:edit, :show, :update]
   before_action :correct_user_edit, only: [:edit, :update, :destroy]
   before_action :logged_in_user, only: [:new, :edit, :update, :destroy]
-
+​
   def index
     @products = Product.all
   end
-
+​
   def new
     @product = Product.new
     @categories = Category.preload(:sizes).order(:name)
     @product.product_images.build
-    @product.product_sizes.build
+​
+    @categories.each do |category|
+      category.sizes.each do |size|
+        @product.product_sizes.build(size_id: size.id, quantity: 0)
+      end
+    end
   end
-
+​
   def home
     @products = Product.paginate(page: params[:page])
   end
-
+​
   def edit
     @categories = Category.preload(:sizes)
+​
+    @categories.each do |category|
+      category.sizes.each do |size|
+        @product.product_sizes.detect do |ps|
+          ps.size_id == size.id
+        end || @product.product_sizes.build(size_id: size.id, quantity: 0)
+      end
+    end
   end
-
+​
   def show
   end
-
+​
   def update
     if @product.update(product_params)
        redirect_to @product
@@ -33,45 +46,47 @@ class ProductsController < ApplicationController
       render "edit"
     end
   end
-
+​
   def create
     @product = Product.new product_params
     @product.user_id = current_user.id
+    @categories = Category.preload(:sizes).order(:name)
+​
     if @product.save
       redirect_to @product
       flash[:success] = "You have created a new product"
     else
-      flash[:danger] = "Your product didn't save"
+      flash[:danger] = "Your product didn't save #{@product.errors.full_messages}"
       render "new"
     end
   end
-
+​
   def destroy
     @product.destroy
     flash[:success] = "Product deleted"
     redirect_to user_products_path
   end
-
+​
   def add_outfit_products
     @products = current_user.products
   end
-
+​
   def add_outfit_similar_products
     @products = current_user.products
   end
-
+​
   private
-
+​
   def create_product_images
     params["product"]["product_images_attributes"].each do |index, image|
       ProductImage.create(product_image: image, product_id: @form.product.id)
     end
   end
-
+​
   def set_product
     @product = Product.find(params[:id])
   end
-
+​
   def product_params
      params.require(:product).permit(
       :title,
@@ -81,11 +96,11 @@ class ProductsController < ApplicationController
       :category_id,
       :size_description,
       :shipping_description,
-      product_images_attributes: [:product_image, :_destroy],
-      product_sizes_attributes: [:size_id, :quantity]
+      product_images_attributes: [:product_image, :_destroy, :id],
+      product_sizes_attributes: [:size_id, :quantity, :id]
     )
   end
-
+​
   def correct_user_edit
     if @product = current_user.products.find_by(id: params[:id])
     else
