c4ec5e99c56d4bca38230ad67f670139154afda5
joeyk16 <drknoba@hotmail.com>
2016-02-02 21:43:16 +1000

    product form WIP

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -1,7 +1,6 @@
 class ProductImage < ActiveRecord::Base
   belongs_to :product
 
-  has_attached_file :product_image, styles: { large: "600x600", medium: "250x250", thumb:"100x100#"}
+  has_attached_file :product_image, styles: { large: "600x600", medium: "250x250", thumb:"100x100#"}, keep_old_files: true
   validates_attachment_content_type :product_image, content_type: /\Aimage\/.*\Z/
 end
-
