49911e5ff967e7c99bca8caed31c26d985eb741d
joeyk16 <drknoba@hotmail.com>
2016-03-22 21:30:33 +1000

    refractoring addresses

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,7 +1,5 @@
 class AddressesController < ApplicationController
   before_action :set_address,   only: [:edit, :update, :destroy]
-  before_action :user_has_one_default_shipping_address, only: [:create, :update]
-  before_action :user_has_one_default_billing_address, only: [:create, :update]
   before_action :authenticate_user!
   before_action :correct_user, only: [:edit, :update, :destroy]
 
@@ -60,20 +58,6 @@ class AddressesController < ApplicationController
       :country,
       :user_id
     )
-  end
-
-  def user_has_one_default_shipping_address
-    address = Address.find_by(user_id: current_user, default_devlivery_address: true)
-    if (params[:address][:default_devlivery_address] == "1") && address
-      address.update_attributes(default_devlivery_address: false)
-    end
-  end
-
-  def user_has_one_default_billing_address
-    address = Address.find_by(user_id: current_user, default_billing_address: true)
-    if (params[:address][:default_billing_address] == "1") && address
-      address.update_attributes(default_billing_address: false)
-    end
   end
 
   def correct_user
--- app/models/address.rb
+++ app/models/address.rb
@@ -3,7 +3,24 @@ class Address < ActiveRecord::Base
 
   validates :address_line_1, :suburb, :state, :postcode, :country, :user, presence: true
 
+  before_save :user_has_one_default_devlivery_address
+  before_save :user_has_one_default_billing_address
+
   def address_to_s
     "#{address_line_1}, #{suburb}, #{state}, #{country}, #{postcode}"
+  end
+
+  def user_has_one_default_devlivery_address
+    return unless self.default_devlivery_address == true
+    return unless address = user.addresses.find_by(default_devlivery_address: true)
+    return if address.id == self.id
+    address.update_attributes(default_devlivery_address: false)
+  end
+
+  def user_has_one_default_billing_address
+    return unless self.default_billing_address == true
+    return unless address = user.addresses.find_by(default_billing_address: true)
+    return if address.id == self.id
+    address.update_attributes(default_billing_address: false)
   end
 end
