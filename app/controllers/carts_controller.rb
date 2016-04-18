4ed6455f22287d772f1fa4271d29d015e07d36c3
joeyk16 <drknoba@hotmail.com>
2016-04-18 22:06:37 +1000

    add cart quantity checks and updates

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,12 +8,23 @@ class CartsController < ApplicationController
 
   def create
     @cart = Cart.new(cart_params)
-    if @cart.save
+    if @cart.save && check_product_quantity?
+      Product.adjust_quantity(@cart.product_id, @cart.size_id, @cart.quantity)
       flash[:success] = "Product added to cart"
     else
       flash[:danger] =  "#{@cart.errors.full_messages}"
     end
     redirect_to :back
+  end
+
+  def check_product_quantity?
+    product_size = Product.find(@cart.product_id).product_sizes.find_by(size_id: @cart.size_id)
+    if product_size.quantity >= @cart.quantity
+      true
+    else
+      @cart.errors.add(:base, "Product doesn't have enough quantity, please choose a lower quantity")
+      false
+    end
   end
 
   def show
